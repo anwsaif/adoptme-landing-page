5e59a5697d8d0b35b67bb18978e4fe3594962da9
Anwar S <anwarsaifullah+code@yandex.com>
2024-06-03 07:09:59 +0000

    fix: use scale to take nav out of screen to prevent clickable links

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import logo from "../assets/brand-logo.png";
 import { Fade as Hamburger } from 'hamburger-react'
 import { Icon } from '@iconify/react';
 
 function Header() {
   const [isOpen, setOpen] = useState(false)
+
   return (
     <header>
       <div className="container mx-auto flex items-center justify-between lg:py-10 px-2">
@@ -14,7 +15,7 @@ function Header() {
         <a href="#">
           <img src={logo} alt="AdoptMe" />
         </a>
-        <div className={"flex max-lg:flex-col  max-lg:fixed max-lg:w-screen max-lg:h-[100dvh] top-0 left-0 items-center max-lg:space-y-6 lg:space-x-40 justify-center lg:justify-between z-40 bg-white transition duration-400 ease-in-out lg:opacity-100 " + (isOpen ? " opacity-100 z-40" : "opacity-0 -z-50")}>
+        <nav className={"flex max-lg:flex-col  max-lg:fixed max-lg:w-screen max-lg:h-[100dvh] top-0 left-0 items-center max-lg:space-y-6 lg:space-x-40 justify-center lg:justify-between bg-white transition duration-300 ease-in-out lg:opacity-100 origin-top " + (isOpen ? " opacity-100 z-40 scale-1" : "opacity-0 -z-50 scale-0")}>
 
           <ul className="flex items-center justify-between max-lg:space-y-4 lg:space-x-12 max-md:flex-col ">
             <li>
@@ -34,10 +35,10 @@ function Header() {
             </li>
           </ul>
           <div className="flex space-x-3">
-            <button>Sign in</button>
-            <button>Sign up</button>
+            <a href="#">Sign in</a>
+            <a href="#">Sign up</a>
           </div>
-        </div>
+        </nav>
         <button type="button" className="lg:hidden">
           <Icon icon="cil:search" width={24} />
         </button>
