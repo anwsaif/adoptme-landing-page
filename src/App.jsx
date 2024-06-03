57e5961b602f1410468ef6f21880e964862fd03a
Anwar S <anwarsaifullah+code@yandex.com>
2024-06-03 14:20:53 +0000

    feat: add bg-gradient

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from './sections/Header'
 function App() {
   return (
     <>
+      <div className='bg-gradient-to-b from-[rgba(195,222,255,0.33)] h-[100dvh] via-[rgba(216,216,216,0)] to-[rgba(190,220,255,0.22)]  -z-50'></div>
       <Header />
     </>
   )
