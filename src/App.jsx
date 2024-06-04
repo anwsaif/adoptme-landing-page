cdb7717884cce0a29deb06573d1c199509edbdfc
Anwar S <anwarsaifullah+code@yandex.com>
2024-06-04 06:18:25 +0000

    style: move the background-radient to Hero

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,8 @@ import Hero from './sections/Hero'
 function App() {
   return (
     <>
-      <div className='min-h-[97vh] relative'>
-        <Header />
-        <div className="bg-gradient-to-b from-[rgba(195,222,255,0.33)] h-[100dvh] via-[rgba(216,216,216,0)] to-[rgba(190,220,255,0.22)] absolute w-screen -z-40"></div>
-        <Hero />
-      </div>
+      <Header />
+      <Hero />
     </>
   )
 }
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -12,7 +12,7 @@ const supportFileNames = [
 
 function Hero() {
   return (
-    <section>
+    <section className="bg-gradient-to-b from-[rgba(195,222,255,0.33)] via-[rgba(216,216,216,0)] to-[rgba(190,220,255,0.22)] min-h-screen">
       <div className="h-16"></div>
       <div className="container mx-auto flex flex-col-reverse items-center justify-between px-3 gap-y-8 lg:flex-row lg:px-4 xl:px-0 lg:py-10 xl:py-28">
         <div className="flex flex-col space-y-4 lg:space-y-8">
