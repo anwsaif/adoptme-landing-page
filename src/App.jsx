d478b8c5f6d8dc2bbd6618a86fb8d1069b7a41f2
Anwar S <anwarsaifullah+code@yandex.com>
2024-06-05 07:47:11 +0000

    feat: add lazy and suspense component loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-import Community from './sections/Community'
-import Header from './sections/Header'
-import Hero from './sections/Hero'
+import { Suspense, lazy } from 'react'
+const Adoption = lazy(() => import('./sections/Adoption'))
+const Community = lazy(() => import('./sections/Community'))
+const Header = lazy(() => import('./sections/Header'))
+const Hero = lazy(() => import('./sections/Hero'))
+const Stories = lazy(() => import('./sections/Stories'))
+const Footer = lazy(() => import('./sections/Footer'))
 
 function App() {
   return (
     <>
+      <Suspense fallback= {<div>Loading...</div>}>
       <Header />
       <Hero />
       <Community />
+      <Adoption />
+      <Stories />
+      <Footer />
+      </Suspense>
     </>
   )
 }
