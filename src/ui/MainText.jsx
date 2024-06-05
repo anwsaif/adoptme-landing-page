9ecc7cba51244d71a2d0b27847ab3c6ef77b5fa4
Anwar S <anwarsaifullah+code@yandex.com>
2024-06-05 07:50:46 +0000

    style(MainText): fix spacing

--- src/ui/MainText.jsx
+++ src/ui/MainText.jsx
@@ -3,9 +3,9 @@ import Title from './Title'
 import Slogan from './Slogan'
 import Description from './Description'
 
-function MainText({title, slogan, description, descClass = '', className}) {
+function MainText({title, slogan, description, descClass = '', className = ''}) {
   return (
-    <div className={'flex flex-col gap-y-3 lg:gap-y-6 max-w-[485px] ' + className}>
+    <div className={'flex flex-col gap-y-3 lg:gap-y-6 ' + className}>
       {title && <Title title={title} />}
       {slogan && <Slogan slogan={slogan} />}
       {description && <Description text={description} className={descClass} />}
