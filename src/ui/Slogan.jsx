8f28f946f334d90a24c58a5d6e722862f0834b2e
Anwar S <anwarsaifullah+code@yandex.com>
2024-06-05 07:51:09 +0000

    style: add max-w for Slogan

--- src/ui/Slogan.jsx
+++ src/ui/Slogan.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Slogan({slogan}) {
   return (
-    <div className="text-dark text-3xl lg:text-4xl">{slogan}</div>
+    <div className="text-dark text-3xl lg:text-4xl max-w-[485px]">{slogan}</div>
   )
 }
 
