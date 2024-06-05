dabdef5f5ea78e526cf005ed8d002cfdcfc74f91
Anwar S <anwarsaifullah+code@yandex.com>
2024-06-05 06:52:37 +0000

    feat: add className to MainText

--- src/ui/MainText.jsx
+++ src/ui/MainText.jsx
@@ -3,9 +3,9 @@ import Title from './Title'
 import Slogan from './Slogan'
 import Description from './Description'
 
-function MainText({title, slogan, description, descClass}) {
+function MainText({title, slogan, description, descClass = '', className}) {
   return (
-    <div className='flex flex-col gap-y-3 lg:gap-y-6'>
+    <div className={'flex flex-col gap-y-3 lg:gap-y-6 max-w-[485px] ' + className}>
       {title && <Title title={title} />}
       {slogan && <Slogan slogan={slogan} />}
       {description && <Description text={description} className={descClass} />}
