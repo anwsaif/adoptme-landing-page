d4392633af68b8842a34a7c753559ef98fa1c48f
Anwar S <anwarsaifullah+code@yandex.com>
2024-06-10 06:59:11 +0700

    feat: add filter adoption button

--- src/sections/Adoption.jsx
+++ src/sections/Adoption.jsx
@@ -4,12 +4,18 @@ import MainText from '../ui/MainText'
 function Adoption() {
   return (
     <section id="adoption">
-      <div className="container mx-auto flex flex-col gap-y-11 px-3 py-9 lg:gap-y-20 lg:px-4 xl:px-0 lg:py-[4.5rem]">
+      <div className="container mx-auto flex flex-col gap-y-11 px-3 py-9 lg:gap-y-20 lg:px-4 lg:py-[4.5rem] xl:px-0">
         <MainText 
           title={'adoption pet'}
           slogan={'Adopt the pet that has waited too long'}
           description={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. It ac purus eget. Sit dolor est proin vestibulum, duis. im dolor sit amet, consectetur adipisci.'}
         />
+        <div className="flex items-center justify-between">
+
+          <button>
+            Filters
+            <Icon icon={'mi:filter'} />
+          </button>
       </div>
     </section>
   )
