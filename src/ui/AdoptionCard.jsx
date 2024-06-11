63a722ea5bd7e9dca1041972438fbdfa987a4c80
Anwar S <anwarsaifullah+code@yandex.com>
2024-06-11 12:19:44 +0000

    feat: add dynamic AdoptionCard

--- src/ui/AdoptionCard.jsx
+++ src/ui/AdoptionCard.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
-function AdoptionCard() {
+function AdoptionCard(props) {
+  const {name, days, age, img} = props
+  const imgSrc = `/src/assets/adoptions/${img}`
   return (
     <div className="hover:drop-shadow-primary hover:bg-[#FEFEFE] p-1 lg:p-2 transition max-w-[297px] snap-start shrink-0">
-      <div className="relative bg-[url('/src/assets/adoptions/bruno.png')] bg-cover bg-no-repeat aspect-[281/333]">
+      <div className={`relative bg-cover bg-no-repeat aspect-[281/333]`} style={{"backgroundImage": `url(${imgSrc})`}}>
         <button className='bg-primary text-white w-7/12 py-4 lg:py-8 rounded-[3px]'>Adopt</button>
-        <span>180 days waiting</span>
+        <span>{days} days waiting</span>
       </div>
     </div>
   )
