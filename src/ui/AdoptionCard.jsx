5cc581fd6e0bcfc0213085fb916a78a08a248da3
Anwar S <anwarsaifullah+code@yandex.com>
2024-06-11 20:52:46 +0700

    style: layout the adoption card items

--- src/ui/AdoptionCard.jsx
+++ src/ui/AdoptionCard.jsx
@@ -4,10 +4,14 @@ function AdoptionCard(props) {
   const {name, days, age, img} = props
   const imgSrc = `/src/assets/adoptions/${img}`
   return (
-    <div className="hover:drop-shadow-primary hover:bg-[#FEFEFE] p-1 lg:p-2 transition max-w-[297px] snap-start shrink-0">
-      <div className={`relative bg-cover bg-no-repeat aspect-[281/333]`} style={{"backgroundImage": `url(${imgSrc})`}}>
-        <button className='bg-primary text-white w-7/12 py-4 lg:py-8 rounded-[3px]'>Adopt</button>
-        <span>{days} days waiting</span>
+    <div className="flex w-full max-w-[297px] shrink-0 snap-start flex-col items-center p-1 transition hover:bg-[#FEFEFE] hover:drop-shadow-primary lg:p-2">
+      <div className={`flex items-center group flex-col justify-end bg-cover bg-no-repeat aspect-[281/333] w-full`} style={{"backgroundImage": `url(${imgSrc})`}}>
+        <button className='mb-[5em] w-1/3 rounded-[3px] bg-primary py-2 text-white hover:-translate-y-2 hover:!bg-primary hover:!text-white group-hover:opacity-100 lg:w-2/3 lg:py-4 lg:opacity-0'>Adopt</button>
+        <span className='text-base text-white transition duration-200 hover:text-dark lg:text-xl'>{days} days waiting</span>
+      </div>
+      <div className='my-2 flex flex-col gap-2 text-center text-sm text-dark lg:my-4 lg:gap-4 lg:text-lg'>
+        <span>{name}</span>
+        <span>{age} old</span>
       </div>
     </div>
   )
