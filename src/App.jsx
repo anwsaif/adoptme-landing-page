caa8156b261f0d949084daadd6919ca9ba6e311e
Anwar S <anwarsaifullah+code@yandex.com>
2024-06-14 06:51:07 +0700

    feat: add aos initialization

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-import { Suspense, lazy } from 'react'
+import { Suspense, lazy, useEffect } from 'react'
 import Loading from './ui/Loading'
+import Aos from 'aos'
 const Adoption = lazy(() => import('./sections/Adoption'))
 const Community = lazy(() => import('./sections/Community'))
 const Header = lazy(() => import('./sections/Header'))
 const Hero = lazy(() => import('./sections/Hero'))
 const Stories = lazy(() => import('./sections/Stories'))
 const Footer = lazy(() => import('./sections/Footer'))
+import 'aos/dist/aos.css'
 
 function App() {
+  useEffect(() => {
+    Aos.init({
+      duration: 600,
+      once: true,
+    })
+  })
   return (
     <>
       <Suspense fallback= {<Loading />}>
